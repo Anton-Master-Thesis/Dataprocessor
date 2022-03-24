af28567b54947f6e1bb3d11ff34df8913cffcaca
Froopas <anton.johansson@infineon.com>
2022-03-24 09:12:38 +0100

    orchestration process works

--- Arrowhead/Orchestration/Orchestration.py
+++ Arrowhead/Orchestration/Orchestration.py
@@ -1,51 +1,75 @@
+from asyncio.windows_events import NULL
 from requests_pkcs12 import post, get
+import urllib3
+import warnings
 import requests
 import json
 import os
 
-basepath = os.path.abspath(os.path.dirname(__file__))
-
+# Starts the orchestration process
+# Raises Exception if the http resonse code from the orchestrator is not 2xx or 3xx
 def orchestrate():
-    config = loadConfig()
+    config = con.loadConfig()
+    orchConfig = config["orchestrationConfig"]
+    orchRequest = config["orchestrationRequest"]
 
-    orchConfig = config["orchestration"]
-
+    # build the adress to request
     adress = orchConfig["ip"] + ":" + str(orchConfig["port"]) + "/orchestrator/orchestration"
 
+    # build the orchestration request payload
     payload = {}
-    payload["requesterSystem"] = config["system"]
-    payload["requestedService"] = config["requestedService"]
-    payload["orchestrationFlags"] = config["orchestrationFlags"]
+    payload["requesterSystem"] = orchRequest["system"]
+    payload["requestedService"] = orchRequest["requestedService"]
+    payload["orchestrationFlags"] = orchRequest["orchestrationFlags"]
 
-    print(json.dumps(payload))
-
+    # check if we need to use http or https
     if orchConfig["secure"]:
+        # Need to disable warning because AH main cert is not trusted
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        # Need to not verify cert because AH main cert is not trusted (verify=False)
         response = post("https://" + adress, pkcs12_filename=orchConfig["Cloud_cert"], pkcs12_password=orchConfig["cert_pass"], verify=False, json=payload)
+        # Reset warnings in case we need to verify other requests
+        warnings.resetwarnings()
     else :
         response = post("http://" + adress, json=payload)
 
-    print(response.text)
+    # Check if request was successful
     if response.status_code != requests.codes.ok:
         raise Exception(response.status_code, response.json())
 
-def testConnection():
-    config = loadConfig()
+    return response.json()
 
-    orchConfig = config["orchestration"]
+# Tests the connection via the /echo endpoint of the orchestrator
+# Raises exception if connection could not be established
+def testConnection():
+    config = con.loadConfig()
+
+    orchConfig = config["orchestrationConfig"]
 
     adress = orchConfig["ip"] + ":" + str(orchConfig["port"]) + "/orchestrator/echo"
     if orchConfig["secure"]:
+         # Need to disable warning because AH main cert is not trusted
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        # Need to not verify cert because AH main cert is not trusted (verify=False)
         response = get("https://" + adress, pkcs12_filename=orchConfig["Cloud_cert"], pkcs12_password=orchConfig["cert_pass"], verify=False)
+        # Reset warnings in case we need to verify other requests
+        warnings.resetwarnings()
     else:
         response = get("http://" + adress)
 
+    # Check if request was successful
     if response.status_code != requests.codes.ok:
-        return False
-    
-    return True
+        raise Exception(response.status_code, "Unable to connect to the orchestrator")
 
-def loadConfig():
-    orchConfigpath = os.path.abspath(os.path.join(basepath, "OrchestrationConfig.json"))
-    with open(orchConfigpath, 'r') as f:
-        configJson = json.load(f)
-    return configJson
+class Config:
+    orchestrationConfig = {}
+    basepath = os.path.abspath(os.path.dirname(__file__))
+    # Load the orchestration configuration file which describes which service to connect to and where/how to contact the orchestrator
+    def loadConfig(self):
+        if not self.orchestrationConfig:
+            orchConfigpath = os.path.abspath(os.path.join(self.basepath, "OrchestrationConfig.json"))
+            with open(orchConfigpath, 'r') as f:
+                self.orchestrationConfig = json.load(f)
+        return self.orchestrationConfig
+
+con = Config()
--- run.py
+++ run.py
@@ -4,6 +4,5 @@ from Arrowhead.Orchestration import *
 
 if __name__ == "__main__":
     #app.run(debug=True, host="0.0.0.0")
-    if not testConnection():
-        raise Exception("Unable to connect to orchestrator")
+    testConnection()
     orchestrate()
